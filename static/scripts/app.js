511f296fcd942a1ed238e7a24769f742179c34fc
jhoover4 <jordan@hoovermld.com>
2019-01-06 17:58:19 -0600

    Add todoItem Vue component.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,8 +1,8 @@
-// Creates a tdo object
-function Todo(id, name) {
+// Creates a _todo object
+function Todo(id, name, created) {
     this.id = id;
     this.name = name;
-    this.edited = false;
+    this.created = created;
     this.completed = false;
 }
 
@@ -11,23 +11,35 @@ Vue.component('todo-item', {
     props: {
         todo: Object
     },
+    data: function () {
+        return {
+            isEditing: false,
+            editMade: false
+        }
+    },
     template: `
-    <div class="item">
-        <input type="checkbox" v-on:click="markCompleted"/>
-        <span></span>
-        <label v-on:click="editing"></label>
-        <input class="editing-label" type="text" v-bind:value="todo.name"/>
+    <div class="item" :class="{ 'editing-item': isEditing, 'completed': todo.completed, 'edited': editMade }">
+        <input type="checkbox" :checked="todo.completed"/>
+        <span @click="markCompleted"></span>
+        <label v-show="!isEditing" @click="editing">{{ todo.name }}</label>
+        <input class="editing-label" v-show="isEditing" @focusout="" @blur="doneEditing(), $emit('editing-complete')" type="text" :value="todo.name"/>
         <div class="actions">
-            <a href="" v-on:click="updateTodo(todo.id, todo.name)">Save</a>
-            <a href="" class="delete" v-on:click="deleteTodo(todo.id)">Delete</a>
+            <button @click="$emit('save')">Save</button>
+            <button class="delete" @click="$emit('delete')">Delete</button>
         </div>
     </div>`,
     methods: {
         editing: function () {
-
+            this.isEditing = !this.isEditing;
+            this.editMade = true;
+        },
+        doneEditing: function () {
+            this.isEditing = false;
         },
         markCompleted: function () {
-
+            this.todo.completed = !this.todo.completed;
+            this.editMade = true;
+            this.$emit('todo-completed');
         }
     }
 });
@@ -36,53 +48,47 @@ var app = new Vue({
     el: '#app',
     data: {
         todos: [],
-        currentTodo: null,
         allTodoRoute: '/api/v1/todos',
-        singleTodoRoute: '/api/v1/todo/'
+        singleTodoRoute: '/api/v1/todos/'
     },
     methods: {
         convertToTodo: function (todoJson) {
-            let todo = new Todo(todoJson.id, todoJson.name);
-
-            return todo;
+            return new Todo(todoJson.id, todoJson.name, false);
+        },
+        updateTempTodo: function (todo) {
+            todo.name = event.target.value;
+        },
+        sortTodos: function (todos) {
+            this.todos.sort((a, b) => ((a.completed === b.completed) ? 0 : a.completed ? 1 : -1));
+        },
+        createFetchHeaders: function (todo, method, username = 'jhoover', password = 'test') {
+            return {
+                method: method,
+                headers: {
+                    'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'Authorization': 'Basic ' + btoa(username + ":" + password)
+                },
+                body: `id=${todo.id}&name=${todo.name}`
+            };
         },
         getAllTodos: function () {
             fetch(this.allTodoRoute)
                 .then(response => response.json())
-                .then(jsonData => (jsonData.forEach(el => {
-                    this.todos.push(this.convertToTodo(el));
-                })).catch(error => console.log('Request failed', error)));
+                .then((jsonData) => {
+                    jsonData.forEach(el => {
+                        this.todos.push(this.convertToTodo(el));
+                    })
+                }).catch(error => console.log('Request failed', error));
         },
-        getSingleTodo: function (id) {
-            let todo;
-
-            fetch(this.$data.singleTodoRoute + id)
-                .then(response => response.json())
-                .then(jsonData => (todo = jsonData))
-                .catch(error => console.log('Request failed', error));
-
-            return todo;
+        deleteTodo: function (todo) {
+            if (todo.created) {
+                this.deleteTempTodo(todo.id);
+            } else {
+                this.deleteServerTodo(todo.id);
+                this.deleteTempTodo(todo.id);
+            }
         },
-        updateTodo: function (id, name) {
-            let todo;
-            const options = {
-                method: 'put',
-                headers: {
-                    'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-                },
-                body: `id=${id}&name=${name}`
-            };
-
-            fetch(this.$data.singleTodoRoute + id, options)
-                .then(response => response.json())
-                .then(jsonData => (todo = jsonData))
-                .catch(err => {
-                    console.error('Request failed', err)
-                });
-
-            return todo;
-        },
-        deleteTodo: function (id) {
+        deleteServerTodo: function (id) {
             let todo;
             const options = {
                 method: 'delete',
@@ -97,44 +103,52 @@ var app = new Vue({
                 })
         },
         deleteTempTodo: function (id) {
-            this.todos.splice(index, 1);
-        },
-        saveTodos: function () {
-            let filteredTodos = this.todos.filter(function (todo) {
-                if (todo.edited) {
-                    return todo;
+            for (i = 0; i <= this.todos.length; i++) {
+                if (this.todos[i].id === id) {
+                    this.todos.splice(i, 1);
                 }
-            });
-            filteredTodos.forEach(function (todo) {
-                if (todo.id) {
-                    this.updateTodo(todo.id)
-                } else {
-                    this.createTodo(todo.id)
-                }
-            });
+            }
         },
         addTodo: function () {
-            this.$data.todos.push({})
-        },
-        addTodoAPI: function () {
-            let todo;
-            const options = {
-                method: 'post',
-                headers: {
-                    'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-                },
-                body: `id=${id}&name=${name}`
-            };
+            const lastTodoId = this.$data.todos.slice(-1)[0].id;
+            const newTodoId = lastTodoId + 1;
 
-            fetch(this.$data.singleTodoRoute + id, options)
+            const todo = new Todo(newTodoId, "New task!", true);
+            this.todos.push(todo);
+        },
+        saveTodo: function (todo) {
+            if (todo.created) {
+                this.insertTodo(todo);
+            } else {
+                this.updateTodo(todo);
+            }
+        },
+        insertTodo: function (todo) {
+            const options = this.createFetchHeaders(todo, 'post');
+
+            fetch(this.$data.allTodoRoute, options)
                 .then(response => response.json())
                 .then(jsonData => (todo = jsonData))
                 .catch(err => {
                     console.error('Request failed', err)
                 });
 
+            todo.created = false;
+
             return todo;
-        }
+        },
+        updateTodo: function (todo) {
+            const options = this.createFetchHeaders(todo, 'put');
+
+            fetch(this.$data.singleTodoRoute + todo.id, options)
+                .then(response => response.json())
+                .catch(err => {
+                    console.error('Request failed', err);
+                    return false
+                });
+
+            return true;
+        },
     },
     mounted() {
         this.getAllTodos();
